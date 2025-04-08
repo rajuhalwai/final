f743862583e5ade802571e8d4dcff6e210c08a62
rajuhalwai <savsanipintu1111@gmail.com>
2025-04-08 06:09:27 +0530

    Asp

---
+++ asp/crud/Default.aspx
@@ -0,0 +1,101 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="crud.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <table>
+                <tr>
+                    <td>id</td>
+                    <td>
+                        <asp:TextBox runat="server" ID="txtid" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>name</td>
+                    <td>
+                        <asp:TextBox runat="server" ID="txtnm" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>surname</td>
+                    <td>
+                        <asp:TextBox runat="server" ID="txtsnm" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>email</td>
+                    <td>
+                        <asp:TextBox runat="server" ID="txtemail" TextMode="Email" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>password</td>
+                    <td>
+                        <asp:TextBox runat="server" ID="txtpass" TextMode="Password" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>mobile no</td>
+                    <td>
+                        <asp:TextBox runat="server" ID="txtmn" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>gender</td>
+                    <td>
+                        <asp:RadioButtonList runat="server" ID="txtgender">
+                            <asp:ListItem Text="Male" Value="gender" />
+                            <asp:ListItem Text="Female" Value="gender" />
+                        </asp:RadioButtonList>
+                    </td>
+                </tr>
+                <tr>
+                    <td>date</td>
+                    <td>
+                        <asp:TextBox runat="server" ID="txtdt" TextMode="Date" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Button Text="insert" runat="server" ID="btnins" OnClick="btnins_Click" />
+                        <asp:Button Text="show" runat="server" ID="btnshow" OnClick="btnshow_Click" />
+                        <asp:button text="update" runat="server" id="btnupdate" OnClick="btnupdate_Click" />
+                        <asp:button text="delete" runat="server" id="btndlt" OnClick="btndlt_Click" />
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <asp:GridView ID="GridView1" runat="server" Width="490px" AutoGenerateColumns="False" DataKeyNames="id" CellPadding="4" ForeColor="#333333" GridLines="None">
+            <AlternatingRowStyle BackColor="White" />
+            <Columns>
+                <asp:BoundField DataField="id" HeaderText="id" InsertVisible="False" ReadOnly="True" SortExpression="id" />
+                <asp:BoundField DataField="name" HeaderText="name" SortExpression="name" />
+                <asp:BoundField DataField="surname" HeaderText="surname" SortExpression="surname" />
+                <asp:BoundField DataField="email" HeaderText="email" SortExpression="email" />
+                <asp:BoundField DataField="password" HeaderText="password" SortExpression="password" />
+                <asp:BoundField DataField="mobileno" HeaderText="mobileno" SortExpression="mobileno" />
+                <asp:BoundField DataField="gender" HeaderText="gender" SortExpression="gender" />
+                <asp:BoundField DataField="date" HeaderText="date" SortExpression="date" />
+            </Columns>
+            <EditRowStyle BackColor="#2461BF" />
+            <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#EFF3FB" />
+            <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscendingCellStyle BackColor="#F5F7FB" />
+            <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+            <SortedDescendingCellStyle BackColor="#E9EBEF" />
+            <SortedDescendingHeaderStyle BackColor="#4870BE" />
+        </asp:GridView>
+         <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:conn %>" SelectCommand="SELECT * FROM [demo]"></asp:SqlDataSource>
+   
+    </form>
+</body>
+</html>
---
+++ asp/crud/Default.aspx.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace crud
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        string constr = ConfigurationManager.ConnectionStrings["conn"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            showdata();
+        }
+        private void showdata()
+        {
+            try
+            {
+                SqlConnection cn = new SqlConnection(constr);
+                cn.Open();
+                SqlCommand cmd = new SqlCommand("select * from demo", cn);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                if (dt.Rows.Count > 0)
+                {
+                    GridView1.DataSource = dt;
+                    GridView1.DataBind();
+                }
+                else
+                {
+                    Response.Write("no data found");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        protected void btnins_Click(object sender, EventArgs e)
+        {
+            adddata();
+        }
+
+        private void adddata()
+        {
+            try
+            {
+                SqlConnection cn = new SqlConnection(constr);
+                cn.Open();
+                SqlCommand cmd = new SqlCommand("insert into demo (name,surname,email,password,mobileno,gender,date) values (@nm,@snm,@email,@pass,@mn,@gender,@dt)", cn);
+                cmd.Parameters.AddWithValue("@nm", txtnm.Text);
+                cmd.Parameters.AddWithValue("@snm", txtsnm.Text);
+                cmd.Parameters.AddWithValue("@email", txtemail.Text);
+                cmd.Parameters.AddWithValue("@pass", txtpass.Text);
+                cmd.Parameters.AddWithValue("@mn", txtmn.Text);
+                cmd.Parameters.AddWithValue("@gender", txtgender.Text);
+                cmd.Parameters.AddWithValue("@dt", DateTime.Parse(txtdt.Text));
+                cmd.ExecuteNonQuery();
+                cn.Close();
+                showdata();
+                ClearFields();
+                txtnm.Text = "";
+                txtsnm.Text = "";
+                txtemail.Text = "";
+                txtpass.Text = "";
+                txtmn.Text = "";
+                txtgender.Text = "";
+                txtdt.Text = "";
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        protected void btnshow_Click(object sender, EventArgs e)
+        {
+            finddata();
+        }
+
+        private void finddata()
+        {
+            try
+            {
+                SqlConnection cn = new SqlConnection(constr);
+                cn.Open();
+                SqlCommand cmd = new SqlCommand("select * from demo where id=@id", cn);
+                cmd.Parameters.AddWithValue("@id", txtid.Text);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                if (dt.Rows.Count > 0)
+                {
+                    txtnm.Text = dt.Rows[0]["name"].ToString();
+                    txtsnm.Text = dt.Rows[0]["surname"].ToString();
+                    txtemail.Text = dt.Rows[0]["email"].ToString();
+                    txtpass.Text = dt.Rows[0]["password"].ToString();
+                    txtmn.Text = dt.Rows[0]["mobileno"].ToString();
+                    txtgender.Text = dt.Rows[0]["gender"].ToString();
+                    txtdt.Text = dt.Rows[0]["date"].ToString();
+                }
+                else
+                {
+                    Response.Write("no data found");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        protected void btnupdate_Click(object sender, EventArgs e)
+        {
+            updatedata();
+        }
+
+        private void updatedata()
+        {
+            try
+            {
+                SqlConnection cn = new SqlConnection(constr);
+                cn.Open();
+                SqlCommand cmd = new SqlCommand("update demo set name=@nm,surname=@snm,email=@email,password=@pass,mobileno=@mn,gender=@gender,date=@dt", cn);
+                cmd.Parameters.AddWithValue("@nm", txtnm.Text);
+                cmd.Parameters.AddWithValue("@snm", txtsnm.Text);
+                cmd.Parameters.AddWithValue("@email", txtemail.Text);
+                cmd.Parameters.AddWithValue("@pass", txtpass.Text);
+                cmd.Parameters.AddWithValue("@mn", txtmn.Text);
+                cmd.Parameters.AddWithValue("@gender", txtgender.Text);
+                cmd.Parameters.AddWithValue("@dt", txtdt.Text);
+                cmd.ExecuteNonQuery();
+                cn.Close();
+                showdata();
+                ClearFields();
+                txtnm.Text = "";
+                txtsnm.Text = "";
+                txtemail.Text = "";
+                txtpass.Text = "";
+                txtmn.Text = "";
+                txtgender.Text = "";
+                txtdt.Text = "";
+                txtid.Text = "";
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        protected void btndlt_Click(object sender, EventArgs e)
+        {
+            deletedata();
+        }
+
+        private void deletedata()
+        {
+            try
+            {
+                SqlConnection cn = new SqlConnection(constr);
+                cn.Open();
+                SqlCommand cmd = new SqlCommand("delete from demo where id=@id", cn);
+                cmd.Parameters.AddWithValue("@id", txtid.Text);
+                cmd.ExecuteNonQuery();
+                cn.Close();
+                showdata();
+                ClearFields();
+                txtid.Text = "";
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        private void ClearFields()
+        {
+            txtid.Text = "";
+            txtnm.Text = "";
+            txtsnm.Text = "";
+            txtemail.Text = "";
+            txtpass.Text = "";
+            txtmn.Text = "";
+            txtgender.Text = "";
+            txtdt.Text = "";
+        }
+    }
+}
