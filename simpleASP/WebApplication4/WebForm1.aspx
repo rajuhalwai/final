effd5a4ea4791c8e5db26687e7374a3ecb442a91
rajuhalwai <savsanipintu1111@gmail.com>
2025-04-08 06:36:55 +0530

    ASP

---
+++ simpleASP/WebApplication4/WebForm1.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication4.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+
+                <tr>
+                    <td>id</td>
+                    <td>
+                        <asp:TextBox runat="server" ID="txtid" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>name</td>
+                    <td>
+                        <asp:TextBox runat="server" ID="txtnm" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>address</td>
+                    <td>
+                        <asp:TextBox runat="server" ID="txtadd" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>city</td>
+                    <td>
+                        <asp:TextBox runat="server" ID="txtct" />
+                    </td>
+                </tr>
+                
+                    <td>
+                        <asp:Button Text="insert" runat="server" ID="btnins" OnClick="btnins_Click" />
+                        <asp:Button Text="show" runat="server" ID="btnshow" OnClick="btnshow_Click" />
+                        <asp:Button Text="update" runat="server" ID="btnupt" OnClick="btnupt_Click" />
+                        <asp:Button Text="delete" runat="server" ID="btndlt" OnClick="btndlt_Click" />
+                    </td>
+            </table>
+        </div>
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="id">
+            <Columns>
+                <asp:BoundField DataField="id" HeaderText="id" InsertVisible="False" ReadOnly="True" SortExpression="id" />
+                <asp:BoundField DataField="name" HeaderText="name" SortExpression="name" />
+                <asp:BoundField DataField="address" HeaderText="address" SortExpression="address" />
+                <asp:BoundField DataField="city" HeaderText="city" SortExpression="city" />
+            </Columns>
+        </asp:GridView>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:examConnectionString3 %>" SelectCommand="SELECT * FROM [exam]"></asp:SqlDataSource>
+
+    </form>
+</body>
+</html>
---
+++ simpleASP/WebApplication4/WebForm1.aspx.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WebApplication4
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string constr = ConfigurationManager.ConnectionStrings["conn"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            showdata();
+        }
+        private void showdata()
+        {
+            try
+            {
+                SqlConnection cn = new SqlConnection(constr);
+                cn.Open();
+                SqlCommand cmd = new SqlCommand("select * from test", cn);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                if (dt.Rows.Count > 0)
+                {
+                    GridView1.DataSource = dt;
+                    GridView1.DataBind();
+                }
+                else
+                {
+                    Response.Write("error");
+                }
+                cn.Close();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+        protected void btnins_Click(object sender, EventArgs e)
+        {
+            insertdata();
+        }
+
+        private void insertdata()
+        {
+            try
+            {
+                SqlConnection cn = new SqlConnection(constr);
+                cn.Open();
+                SqlCommand cmd = new SqlCommand("insert into test(name,address,city)values(@nm,@add,@city)", cn);
+                cmd.Parameters.AddWithValue("@nm", txtnm.Text);
+                cmd.Parameters.AddWithValue("@add", txtadd.Text);
+                cmd.Parameters.AddWithValue("@city", txtct.Text);
+                cmd.ExecuteNonQuery();
+                cn.Close();
+                showdata();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+        protected void btnshow_Click(object sender, EventArgs e)
+        {
+            finddata();
+        }
+
+        private void finddata()
+        {
+            try
+            {
+                SqlConnection cn = new SqlConnection(constr);
+                cn.Open();
+                SqlCommand cmd = new SqlCommand("select * from test where id=@id", cn);
+                cmd.Parameters.AddWithValue("@id", txtid.Text);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                if (dt.Rows.Count > 0)
+                {
+                    txtnm.Text = dt.Rows[0]["name"].ToString();
+                    txtadd.Text = dt.Rows[0]["address"].ToString();
+                    txtct.Text = dt.Rows[0]["city"].ToString();
+                }
+                else
+                {
+                    Response.Write("error");
+                }
+                cn.Close();
+                showdata();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        protected void btnupt_Click(object sender, EventArgs e)
+        {
+            updatedata();
+        }
+
+        private void updatedata()
+        {
+            try
+            {
+                SqlConnection cn = new SqlConnection(constr);
+                cn.Open();
+                SqlCommand cmd = new SqlCommand("update test set name=@nm,address=@add,city=@ct where id=@id", cn);
+                cmd.Parameters.AddWithValue("@nm", txtnm.Text);
+                cmd.Parameters.AddWithValue("@add", txtadd.Text);
+                cmd.Parameters.AddWithValue("@ct", txtct.Text);
+                cmd.Parameters.AddWithValue("@id", txtid.Text);
+                cmd.ExecuteNonQuery();
+                cn.Close();
+                showdata();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        protected void btndlt_Click(object sender, EventArgs e)
+        {
+            deletedata();
+        }
+
+        private void deletedata()
+        {
+            try
+            {
+                SqlConnection cn = new SqlConnection(constr);
+                cn.Open();
+                SqlCommand cmd = new SqlCommand("delete from test where id=@id", cn);
+                cmd.Parameters.AddWithValue("@id", txtid.Text);
+                cmd.ExecuteNonQuery();
+                cn.Close();
+                showdata();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
